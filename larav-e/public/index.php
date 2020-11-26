0dc5ec56829e1690e7fe5f4f9a8c00920aa7cf34
Gracieux AHLONSOU <ahlonsougracieux@gmail.com>
2020-11-26 16:33:14 +0100

    index.php remis en place

--- larav-e/public/index.php
+++ larav-e/public/index.php
@@ -1,27 +1,55 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-?>
+define('LARAVEL_START', microtime(true));
 
-<!DOCTYPE html
-<html lang="Fr-fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+/*
+|--------------------------------------------------------------------------
+| Check If Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is maintenance / demo mode via the "down" command we
+| will require this file so that any prerendered template can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
+if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
+    require __DIR__.'/../storage/framework/maintenance.php';
+}
 
-    <link rel="stylesheet" href="../resources/css/app.css">
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-    <title> Larav-e </title>
-</head>
-<body>
-    <h1> Larav-e </h1>
+require __DIR__.'/../vendor/autoload.php';
 
-<footer>
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-</footer>
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
+$kernel = $app->make(Kernel::class);
 
+$response = tap($kernel->handle(
+    $request = Request::capture()
+))->send();
 
-</body>
-</html>
+$kernel->terminate($request, $response);

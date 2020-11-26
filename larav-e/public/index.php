74f46de201fc3dfc4413f99a4905c8c64a1aec68
FlowNight <60231824+FlowNight@users.noreply.github.com>
2020-11-26 16:15:56 +0100

    Update
    News

--- larav-e/public/index.php
+++ larav-e/public/index.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-    <link rel="stylesheet" href="../ressources/css/app.css">
+    <link rel="stylesheet" href="../resources/css/app.css">
 
     <title> Larav-e </title>
 </head>

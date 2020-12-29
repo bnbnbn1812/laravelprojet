d06c7e10e3117e2b9902013e43b35f993fe1f323
FlowNight <alexis.phelippon98@orange.fr>
2020-12-29 12:58:09 +0100

    Update v1.0.3

--- larav-e/resources/views/layouts/header.blade.php
+++ larav-e/resources/views/layouts/header.blade.php
@@ -4,14 +4,14 @@
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
                     <h4 class="text-white">About</h4>
-                    <p class="text-white">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+                    <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam laborum similique possimus sed ullam odit maxime corporis provident corrupti aperiam, assumenda voluptatibus suscipit eius natus consectetur dolorum! Cupiditate, nemo accusantium?</p>
                 </div>
                 <div class="col-sm-4 offset-md-1 py-4">
                     <h4 class="text-white">Contact</h4>
                     <ul class="list-unstyled">
-                        <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                        <li><a href="#" class="text-white">Like on Facebook</a></li>
-                        <li><a href="#" class="text-white">Email me</a></li>
+                        <li><a href="#" class="text-white">Twitter <i class="fab fa-twitter"></i> </a></li>
+                        <li><a href="#" class="text-white">Facebook <i class="fab fa-facebook-f"></i> </a></li>
+                        <li><a href="#" class="text-white">E-mail <i class="fas fa-paper-plane"></i> </a></li>
                     </ul>
                 </div>
             </div>
@@ -21,15 +21,15 @@
         <div class="container d-flex justify-content-between">
             <a href="#" class="navbar-brand d-flex align-items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-                <strong>SHOP NEW MONDE</strong>
+                <strong> SHOP NEW MONDE </strong>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Panier </a>
-                <a class="nav-link" href="#"><i class="fas fa-sign-out-alt"></i> Connexion </a>
+                <a class="nav-link" href="#"> Panier <i class="fa fa-shopping-cart"></i> </a>
+                <a class="nav-link" href="#"> Connexion <i class="fas fa-sign-out-alt"></i> </a>
                 </li>
             </ul>
         </div>

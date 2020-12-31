76ee327c0eac70d7200b38b2de33a0af2733e0d1
Gamra01 <ilyassstudies2017@gmail.com>
2020-12-31 11:30:31 +0100

    router add

--- larav-e/routes/web.php
+++ larav-e/routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-/*/use App\Http\Controllers\MainController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
@@ -28,4 +11,8 @@ Route::get('/',"App\http\controllers\Shop\MainController@index");
 
 Route::get('/produit/voir/{id}','App\http\controllers\Shop\MainController@produit')->name('voir_produit');
 
-Route::get('/categorie','App\http\controllers\Shop\MainController@viewByCategory');
+Route::get('/categorie','App\http\controllers\Shop\MainController@viewByCategory');.
+
+Route::view('/cart','cart');
+Route::view('/checkout','checkout');
+Route::view('/thankyou','thankyou');
